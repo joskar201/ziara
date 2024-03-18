e1799dd4388b4cd67df664fb4f5823c644d41143
Joshua Karanja <joskar201@gmail.com>
2024-03-18 13:34:54 +0300

    remove problematic CustomUser class

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.utils import timezone
 
 
@@ -108,7 +108,7 @@ class ItineraryItem(models.Model):
 
 
 
-class CustomUser(AbstractUser):
-    # Add additional fields here
-    travel_preferences = models.TextField()
-    booking_history = models.TextField()
+# class CustomUser(AbstractUser):
+#     # Add additional fields here
+#     travel_preferences = models.TextField()
+#     booking_history = models.TextField()
